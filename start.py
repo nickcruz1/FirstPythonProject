04643124a017db480a7b57044b37a4b3b9c35ad1
Nick Cruz <nickcruz920@gmail.com>
2023-02-11 22:15:59 -0500

    Added some python code

--- start.py
+++ start.py
@@ -1,5 +1,19 @@
-# Start of First Python File
+# Start of First Python File - My Python Reference
 
+# Creating a variable
 x = 10
-
 print(x)
+
+# Casting (Changing Variable Types)
+x = 10  # currently is an int
+x = str("10")  # now x is a string
+print(x)
+
+x = 1j  # Complex
+
+x = ["apple", "banana", "cherry"]  # list
+
+x = {"name": "John", "age": 30}  # dict
+
+y = True  # bool
+print(y)
